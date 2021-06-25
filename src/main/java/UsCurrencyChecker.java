c4be0107ce4e570177e91f40c730d43d966484bd
Peter Bauer <p.bauer@htl-leonding.ac.at>
2021-06-25 11:03:42 +0200

    Changed exception type to get rid off the sun.reflect dependency

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,8 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.regex.Matcher;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
